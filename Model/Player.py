7a409a767cf06fe3e0404ffcc54bc4c8b3180f87
Marc Takoumba <mtakoumba@gmail.com>
2025-02-01 00:27:25 +0100

    Remise du projet Club d'echec

--- Model/Player.py
+++ Model/Player.py
@@ -11,15 +11,6 @@ class Player:
         """Affiche les informations du joueur."""
         return f"\n {self.first_name} {self.last_name} (ID: {self.national_id}, Né(e) le: {self.dob})\n"
 
-    def __eq__(self, other):
-        """Compare deux joueurs en fonction de leur ID national."""
-        if isinstance(other, Player):
-            return self.national_id == other.national_id
-        return False
-
-    def __hash__(self):
-        """Rend Player hashable en utilisant l'ID national unique."""
-        return hash(self.national_id)
 
     def to_dict(self):
         """Convertit un objet Player en dictionnaire JSON."""
